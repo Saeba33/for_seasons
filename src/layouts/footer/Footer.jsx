b6bac3c2902ea43d4177791114ab0b3089cc28f3
Charles Proust <konoha33@hotmail.fr>
2024-02-08 11:55:19 +0100

    edit footer

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import styles from "./footer.module.css";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className={styles.container}>
       <div className={styles.logo}>Charles Proust</div>
-      <div className={styles.text}>© 2024</div>
+      <div className={styles.text}>4 saisons © {currentYear}</div>
     </div>
   );
 };
