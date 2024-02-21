6f6301920d12f6bcf6924b9b10014b7959c9b8f5
Charles Proust <konoha33@hotmail.fr>
2024-02-21 22:15:32 +0100

    add send mail

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,26 +1,72 @@
 "use client";
 
+import { useState } from "react";
 import styles from "./contact.module.css";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      console.log("Email envoyé avec succès");
+    } else {
+      console.error("Erreur lors de l'envoi de l'email");
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <h1>Contactez-nous</h1>
-      <div className={styles.formContainer}>
-        <form action="" className={styles.form}>
-          <input type="text" placeholder="Nom" />
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Objet" />
-          <textarea
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            placeholder="Votre message"
-          ></textarea>
-          <button>Envoyer</button>
-        </form>
-      </div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Nom"
+        />
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <input
+          type="text"
+          name="subject"
+          value={formData.subject}
+          onChange={handleChange}
+          placeholder="Objet"
+        />
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          cols="30"
+          rows="10"
+          placeholder="Votre message"
+        ></textarea>
+        <button type="submit">Envoyer</button>
+      </form>
     </div>
   );
 };
