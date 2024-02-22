bdf9dfbfadb3d570a92b5128a7e6fc66424e7b6c
Charles Proust <konoha33@hotmail.fr>
2024-02-22 20:33:29 +0100

    fix pbm navbar mobile

--- src/layouts/navbar/links/Links.jsx
+++ src/layouts/navbar/links/Links.jsx
@@ -1,8 +1,8 @@
+import { useContext, useState } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
 import Image from "next/image";
-import { useContext, useState } from "react";
-import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
+import styles from "./links.module.css";
 
 const links = [
   { title: "De saison", path: "/" },
@@ -15,9 +15,18 @@ const links = [
 const Links = ({ handleLogout }) => {
   const [open, setOpen] = useState(false);
   const { isLoggedIn, isAdmin } = useContext(AuthContext);
+  const closeMobileMenu = () => setOpen(false);
 
   return (
     <div className={styles.container}>
+      <Image
+        src="/menu.png"
+        alt="Menu"
+        width={30}
+        height={30}
+        className={styles.menuButton}
+        onClick={() => setOpen(!open)}
+      />
       <div className={styles.links}>
         {links.map((link) => {
           if (!link.requireAdmin || (link.requireAdmin && isAdmin)) {
@@ -25,41 +34,40 @@ const Links = ({ handleLogout }) => {
           }
           return null;
         })}
-        {isLoggedIn && (
+        {isLoggedIn ? (
           <button className={styles.logout} onClick={handleLogout}>
             Déconnexion
           </button>
-        )}
-        {!isLoggedIn && (
+        ) : (
           <NavLink item={{ title: "Connexion", path: "/login" }} />
         )}
       </div>
-      <Image
-        className={styles.menuButton}
-        src="/menu.png"
-        alt=""
-        width={30}
-        height={30}
-        onClick={() => setOpen((prev) => !prev)}
-      />
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => {
-            if (
-              !link.requireAdmin ||
-              (link.requireAdmin && isAdmin)
-            ) {
-              return <NavLink item={link} key={link.title} />;
+            if (!link.requireAdmin || (link.requireAdmin && isAdmin)) {
+              return (
+                <div key={link.title} onClick={closeMobileMenu}>
+                  <NavLink item={link} />
+                </div>
+              );
             }
             return null;
           })}
-          {isLoggedIn && (
-            <button className={styles.logout} onClick={handleLogout}>
+          {isLoggedIn ? (
+            <button
+              className={styles.logout}
+              onClick={() => {
+                handleLogout();
+                closeMobileMenu();
+              }}
+            >
               Déconnexion
             </button>
-          )}
-          {!isLoggedIn && (
-            <NavLink item={{ title: "Connexion", path: "/login" }} />
+          ) : (
+            <div onClick={closeMobileMenu}>
+              <NavLink item={{ title: "Connexion", path: "/login" }} />
+            </div>
           )}
         </div>
       )}
